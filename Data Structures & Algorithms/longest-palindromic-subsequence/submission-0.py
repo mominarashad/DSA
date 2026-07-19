b679e6ff2be4e1ddaba00ce618d9267e2ca8659e
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-07-19 17:54:38 +0500

    Add: longest-palindromic-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        text2=s[::-1]
+        n=len(s)
+        m=len(text2)
+
+        dp=[[0]*(m+1) for _ in range(n+1)]
+
+        for i in range(n+1):
+            for j in range(m+1):
+                if i==0 or j==0:
+                    dp[i][j]=0
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+
+                if s[i-1]==text2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+        return dp[i][j]
+        
