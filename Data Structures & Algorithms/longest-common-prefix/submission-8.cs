8ae8cf51bd13403578f35561829c19c163282661
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-01 15:47:12 +0500

    Add: longest-common-prefix - submission-8

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-8.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        
+        Array.Sort(strs);
+
+        string first=strs[0];
+        string last=strs[strs.Length-1];
+
+        string res="";
+
+        for (int idx=0; idx<first.Length; idx++){
+
+            if (first[idx]!=last[idx]){
+                break;
+            }
+            res+=first[idx];
+        }
+        return res;
+    }
+}
