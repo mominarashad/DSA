eac0004e929d1ea79232f34f229132c9fee834f7
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-01 15:21:48 +0500

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        Dictionary<int,int> freq=new Dictionary<int,int>();
+
+        for(int idx=0; idx<nums.Length; idx++){
+
+            int val=nums[idx];
+            int diff=target-val;
+
+            if (freq.ContainsKey(diff)){
+                return new int[]{freq[diff],idx};
+            }
+            freq[val]=idx;
+        }
+
+        return new int[]{};
+         
+    }
+}
