a8652d3ef10989833aca139185d27b4338b1c78a
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-02 12:00:18 +0500

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+
+        if (s.Length!=t.Length){
+            return false;
+        }
+        
+
+        Dictionary<char,int> freq=new Dictionary<char,int>();
+
+        foreach(char ch in s){
+            if (freq.ContainsKey(ch)){
+                freq[ch]+=1;
+            }
+            else{
+                freq[ch]=1;
+            }
+        }
+
+        foreach(char ch in t){
+            if (freq.ContainsKey(ch)){
+                freq[ch]-=1;
+            }
+            else{
+                return false;
+            }
+
+            if (freq[ch]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
