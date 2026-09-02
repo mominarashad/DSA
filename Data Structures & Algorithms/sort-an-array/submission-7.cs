3f6a6cbf428d65271afe2547babbda719ae695b9
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-02 15:29:23 +0500

    Add: sort-an-array - submission-7

---
+++ Data Structures & Algorithms/sort-an-array/submission-7.cs
@@ -0,0 +1,48 @@
+public class Solution {
+  public int[] SortArray(int[] nums) {
+    
+    int n=nums.Length;
+
+    if (n<=1){
+        return nums;
+    }
+
+    int mid=n/2;
+
+    int[] first_half=SortArray(nums[..mid]);
+    int[] second_half=SortArray(nums[mid..]);
+
+    int i=0;
+    int j=0;
+    int k=0;
+
+    int[] res=new int[n];
+
+    while (i<first_half.Length && j<second_half.Length){
+
+        if (first_half[i]<=second_half[j]){
+            res[k]=first_half[i];
+            i++;
+        }
+        else{
+            res[k]=second_half[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i<first_half.Length){
+        res[k]=first_half[i];
+        i++;
+        k++;
+    }
+
+    while(j<second_half.Length){
+        res[k]=second_half[j];
+        j++;
+        k++;
+    }
+
+    return res;
+  }
+}
