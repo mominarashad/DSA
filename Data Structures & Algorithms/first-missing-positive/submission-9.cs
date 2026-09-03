e92458e13d22fd674ee6b573dfde288e60e61b07
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-03 17:25:30 +0500

    Add: first-missing-positive - submission-9

---
+++ Data Structures & Algorithms/first-missing-positive/submission-9.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+
+        int n=nums.Length;
+
+        for (int i=0; i<n; i++){
+
+            if (nums[i]<=0 || nums[i]>n){
+                nums[i]=n+1;
+            }
+        }
+
+        for (int i=0; i<n; i++){
+            int val=Math.Abs(nums[i]);
+            
+            if (val<=n){
+                nums[val-1]=-Math.Abs(nums[val-1]);
+            }
+        }
+
+        for (int i=0; i<n; i++){
+
+            if (nums[i]>0){
+                return i+1;
+            }
+        }
+        return n+1;
+
+
+        
+    }
+}
