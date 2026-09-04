8dc87fb3e9c6588d60ec8758699fba2b5cc6026c
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-04 12:09:05 +0500

    Add: reverse-string - submission-3

---
+++ Data Structures & Algorithms/reverse-string/submission-3.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+
+        int left=0;
+        int right =s.Length-1;
+
+        while (left<=right){
+            (s[left],s[right])=(s[right],s[left]);
+
+            left+=1;
+            right-=1;
+        }
+        
+    }
+}
