80cae5cfa7a528b63310f139ac10933d78fe11e3
Momina Rashad <120158020+mominarashad@users.noreply.github.com>
2026-09-12 15:41:35 +0500

    Add: string-compression - submission-1

---
+++ Data Structures & Algorithms/string-compression/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+
+        n=len(chars)
+
+        read=0
+        write=0
+
+        while read<n:
+
+            char=chars[read]
+            count=0
+
+            while read<n and chars[read]==char:
+                read+=1
+                count+=1
+
+            chars[write]=char
+            write+=1
+
+            if count>1:
+                for digit in str(count):
+                    chars[write]=digit
+                    write+=1
+
+        return write
+        
